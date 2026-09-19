54e6de46e2e2eacf0673c44cdc591e03ef0cc4db
auggie05 <samaw0928@gmail.com>
2026-09-19 14:45:16 -0400

    Update pmethod.lua
    Allows arguments for command pmethod

--- pmethod.lua
+++ pmethod.lua
@@ -1,4 +1,5 @@
-print("Enter side:")
-for i,v in ipairs(peripheral.getMethods(read()))
+local args = { ... }
+local side = args[1]
+for i,v in ipairs(peripheral.getMethods(side))
     do print(i.."| "..v)
 end
