4b75830a90f8d327b17efbdff8e346532f13f412
auggie05 <samaw0928@gmail.com>
2026-09-19 15:30:33 -0400

    Removed wrong side error

--- root/cmd/pmethod.lua
+++ root/cmd/pmethod.lua
@@ -3,8 +3,6 @@ local side = args[1]
 
 if side == nil
 then print("Error: noArgument. Please add the side you are requesting methods from.")
-elseif side ~= "top" or "right" or "left" or "bottom" or "front" or "back"
-then print("Error: notSide. Please enter a correct side.")
 else
     for i,v in ipairs(peripheral.getMethods(side))
         do print(i.."| "..v)
