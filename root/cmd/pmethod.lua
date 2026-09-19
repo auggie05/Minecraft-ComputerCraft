490230f48db0e3901695fe38384850bf416fc37d
auggie05 <samaw0928@gmail.com>
2026-09-19 16:40:32 -0400

    Update pmethod.lua

--- root/cmd/pmethod.lua
+++ root/cmd/pmethod.lua
@@ -2,7 +2,9 @@ local args = { ... }
 local side = args[1]
 
 if side == nil
-then print("Err: noArgument. Please add the side you are requesting methods from.")
+then 
+    term.setTextColor(colors.red)
+    print("Err: noArgument. Please add the side you are requesting methods from.")
 else 
     for i,v in ipairs(peripheral.getMethods(side))
         do print(i.."| "..v)
