dde3b2d4f7ab265bde168451a5057fcf11e5e02e
auggie05 <samaw0928@gmail.com>
2026-09-19 15:29:37 -0400

    Initial error handling

--- root/cmd/pmethod.lua
+++ root/cmd/pmethod.lua
@@ -1,5 +1,12 @@
 local args = { ... }
 local side = args[1]
-for i,v in ipairs(peripheral.getMethods(side))
-    do print(i.."| "..v)
+
+if side == nil
+then print("Error: noArgument. Please add the side you are requesting methods from.")
+elseif side ~= "top" or "right" or "left" or "bottom" or "front" or "back"
+then print("Error: notSide. Please enter a correct side.")
+else
+    for i,v in ipairs(peripheral.getMethods(side))
+        do print(i.."| "..v)
+    end
 end
