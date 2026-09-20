14ea3c794b9731ded4e26cc040a5d6f48f216e84
auggie05 <samaw0928@gmail.com>
2026-09-20 16:39:18 -0400

    Update xr.open.lua

--- root/cmd/xr/xr.open.lua
+++ root/cmd/xr/xr.open.lua
@@ -3,3 +3,7 @@ local channel = tonumber(args[1])
 
 local modem = peripheral.find("modem") or error("No modem attached", 0)
 modem.open(channel)
+
+if modem.isOpen(channel) == true
+then print("Opened channel "..channel)
+end
