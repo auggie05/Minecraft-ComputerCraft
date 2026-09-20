7c77f8406020ebf624b41a2b71917313c42cfa0a
auggie05 <samaw0928@gmail.com>
2026-09-20 16:37:48 -0400

    Update xr.close.lua
    Changed term.write to print

--- root/cmd/xr/xr.close.lua
+++ root/cmd/xr/xr.close.lua
@@ -5,5 +5,5 @@ local modem = peripheral.find("modem") or error("No modem attached", 0)
 modem.close(channel)
 
 if modem.isOpen(channel) ~= true
-then term.write("Closed channel.")
+then print("Closed channel.")
 end
