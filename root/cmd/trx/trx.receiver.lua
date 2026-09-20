f05f14ddfb4ed98e92b8a11e1c68608525a3108c
auggie05 <samaw0928@gmail.com>
2026-09-20 17:36:03 -0400

    Update to receiver

--- root/cmd/trx/trx.receiver.lua
+++ root/cmd/trx/trx.receiver.lua
@@ -11,7 +11,6 @@ print("Receiving on "..reply)
 
 while true do
     local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-    print(("Message received on side %s on channel %d (reply to %d) from %f blocks away with message %s"):format(
-        side, channel, replyChannel, distance, tostring(message)
+    print(("%d: %s"):format(replyChannel, tostring(message)
     ))
 end
