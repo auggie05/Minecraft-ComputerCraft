81c8b04d92acfa01b084863c750302950cc8e7d6
auggie05 <samaw0928@gmail.com>
2026-09-21 14:08:21 -0400

    Update trx.lua
    Trying to make it one file

--- root/cmd/trx/trx.lua
+++ root/cmd/trx/trx.lua
@@ -1,9 +1,22 @@
 local args = { ... }
 local arg1 = args[1]
+local arg2 = args[2]
+
+function printHelp(command)
+    if command == nil
+    then
+        print("Usage: trx command arguments")
+        print("Try 'trx help command' to see specific help")
+        print("Commands:")
+        print("transmit")
+    elseif command == "transmit"
+    then
+        print("Usage: trx transmit ")
+        print("Try 'trx help command' to see help for specific commands")
+    end
+end
 
 if arg1=="-h" or arg1=="-help" or arg1=="help"
 then 
-
-elseif 
-
+    printHelp(arg2)
 end
